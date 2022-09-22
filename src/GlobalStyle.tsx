cf4be34e369fabced2a26bd0b543d4adbdbc873e
saeda raed <saedaraed19@gmail.com>
2022-09-22 14:04:01 +0300

    setup

---
+++ src/GlobalStyle.tsx
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+import { createGlobalStyle } from "styled-components";
+
+export const Flex = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Container = styled.div`
+ width:80%;
+ margin:0 auto;
+`;
+const GlobalStyle = createGlobalStyle`
+* {
+  margin:0;
+  padding:0;
+  box-sizing: border-box;
+  font-family: "Helvetica Neue LT Std";
+  font-style: normal;
+  font-weight: 300;
+  font-size: 24px;
+  line-height: 24px;
+}
+`;
+
+export default GlobalStyle;
